a39ecc8efbb02e567a0090056231920e1325ebfb
mustafa-tola <mustafatola02@gmail.com>
2023-04-26 22:16:37 +0500

    Updated Get all books endpoints to add filters

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,7 +1,22 @@
 import sql from "@/app/dbConn";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest):Promise<NextResponse> {
-    const books = await sql.unsafe("select * from books");
+export async function GET(req: NextRequest) {
+    let query = "select * from books";
+    if (req.nextUrl.searchParams.has("bookType")) {
+        const type = req.nextUrl.searchParams.get("bookType") as string;
+        const upperedType = (type.charAt(0)).toUpperCase() + (type.slice(1));
+        query += ` where type = '${upperedType}'`
+    }
+    if (req.nextUrl.searchParams.has("bookLimit")) {
+        const limit = parseInt(req.nextUrl.searchParams.get("bookLimit") as string);
+        if (limit >= 1 && limit <= 20) {
+            query += ` limit ${limit}`
+        }
+        else {
+            return NextResponse.json({message: "Books Request limit should be between 1 and 20"},{status: 400});
+        }
+    }
+    const books = await sql.unsafe(query);
     return NextResponse.json(books);
 }
